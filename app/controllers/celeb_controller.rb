c6e9ff8b24937098f606aecc895243d727f10811
Aleksander Avila <aleka.a2001@gmail.com>
2022-11-21 19:39:32 +0300

    added celeb controller

--- app/controllers/celeb_controller.rb
+++ app/controllers/celeb_controller.rb
@@ -1,10 +1,31 @@
 class CelebController < ApplicationController
   def index
+    @celebs = Celeb.all
+  end
+
+  def edit
+    @celeb = Celeb.find(params[:id])
   end
 
   def new
+    @celeb = Celeb.new
   end
 
   def create
+    @celeb = Celeb.new(celebs_params)
+    @celeb.save
+    redirect_to celebs_path(@celebs)
+  end
+
+  def update
+    @celeb = Celeb.find(params[:id])
+    @celeb.update(celebs_params)
+    redirect_to celeb_path(@celebs)
+  end
+
+  private
+
+  def celebs_params
+    params.require(:celeb).permit(:first_name, :last_name, :service, :bio, :photo_url, :price)
   end
 end
