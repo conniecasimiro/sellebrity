56548f470ff88d306da836349515eb86b58848f3
Aleksander Avila <aleka.a2001@gmail.com>
2022-11-22 15:32:50 +0300

    new route root home and skeleton of bookings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   get "home", to: "pages#home"
-  resources :celebs
   resources :users
-  resources :bookings
-  root to: "celebs#home"
+  resources :celebs do
+    resources :bookings
+  end
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
