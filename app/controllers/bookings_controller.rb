4c4c9f2bdcdb0dc6d9390648c12d48bc252962d2
Aleksander Avila <aleka.a2001@gmail.com>
2022-11-22 17:08:48 +0300

    changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,11 @@
 class BookingsController < ApplicationController
   def index
-
+    @celeb = Celeb.find(params[:celeb_id])
+    @bookings = Booking.where(celeb_id: @celeb.id)
   end
 
   def new
+
   end
 
   def create
