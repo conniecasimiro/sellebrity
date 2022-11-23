0ad854a144a88680edd595db94f01943cad64283
harry fleminger <harry.fleminger@hotmail.com>
2022-11-23 11:33:20 +0000

    started user celebs page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
     resources :bookings
   end
   root to: "pages#home"
+
+  get "users/:id/celebs", to: "users#celebs"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
