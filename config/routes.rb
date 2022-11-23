89bea6e043b4a69855640fe758548f5d3afb45bd
Aleksander Avila <aleka.a2001@gmail.com>
2022-11-23 14:27:33 +0300

    user bookings and routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   get "home", to: "pages#home"
-  resources :users
   resources :celebs do
     resources :bookings
   end
+  get '/mybookings', to: 'users#bookings'
+  resources :users, only: [:show]
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
