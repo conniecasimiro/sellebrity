6fe8c43d4ec574b4300219b4e5db63191988f680
harry fleminger <harry.fleminger@hotmail.com>
2022-11-24 15:18:59 +0000

    Finished user-celebs bookings page and added validations

--- app/models/celeb.rb
+++ app/models/celeb.rb
@@ -3,6 +3,13 @@ class Celeb < ApplicationRecord
   has_many :bookings
   has_many :users, through: :bookings
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :service, presence: true
+  validates :bio, presence: true
+  validates :photo_url, presence: true
+
+
   include PgSearch::Model
   pg_search_scope :search_by_name_and_service,
     against: [ :first_name, :last_name, :service ],
