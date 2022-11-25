9d163f842685a2b0e44df922cfeeee444ef11c44
harry fleminger <harry.fleminger@hotmail.com>
2022-11-25 11:02:59 +0000

    Added validations for website urls

--- app/models/celeb.rb
+++ app/models/celeb.rb
@@ -8,6 +8,11 @@ class Celeb < ApplicationRecord
   validates :service, presence: true
   validates :bio, presence: true
   validates :photo_url, presence: true
+  validates :photo_url, format: { with: /(https?:\/\/)?([\w\-])+\.{1}([a-zA-Z]{2,63})([\/\w-]*)*\/?\??([^#\n\r]*)?#?([^\n\r]*)/,
+  message: "Must be a valid instagram" }
+  validates :instagram_url, presence: true
+  validates :instagram_url, format: { with: /(https?:\/\/)?([\w\-])+\.{1}([a-zA-Z]{2,63})([\/\w-]*)*\/?\??([^#\n\r]*)?#?([^\n\r]*)/,
+    message: "Must be a valid instagram" }
 
 
   include PgSearch::Model
